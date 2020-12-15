2082072f2896ed287cd1761afd4d9d44317390eb
Alireza Meskin <alireza.meskin@gmail.com>
2020-12-15 22:24:43 +0100

    Add ObjectSerializer

--- src/main/scala/raft4s/demo/kvstore/KvStateMachine.scala
+++ src/main/scala/raft4s/demo/kvstore/KvStateMachine.scala
@@ -4,11 +4,9 @@ import cats.effect.IO
 import cats.effect.concurrent.Ref
 import raft4s.StateMachine
 import raft4s.demo.kvstore.command.{DeleteCommand, GetCommand, SetCommand}
+import raft4s.demo.kvstore.utils.ObjectSerializer
 import raft4s.protocol.{ReadCommand, WriteCommand}
 import raft4s.storage.Snapshot
-
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
-import java.nio.ByteBuffer
 
 class KvStateMachine(lastIndex: Ref[IO, Long], map: Ref[IO, Map[String, String]]) extends StateMachine[IO] {
 
@@ -39,32 +37,15 @@ class KvStateMachine(lastIndex: Ref[IO, Long], map: Ref[IO, Map[String, String]]
     for {
       items <- map.get
       index <- lastIndex.get
-      bytes = serialize(items)
+      bytes = ObjectSerializer.serialize(items)
     } yield Snapshot(index, bytes)
 
   override def restoreSnapshot(snapshot: Snapshot): IO[Unit] =
     for {
-      _ <- map.set(deserialize(snapshot.bytes))
+      _ <- map.set(ObjectSerializer.deserialize(snapshot.bytes))
       _ <- lastIndex.set(snapshot.lastIndex)
     } yield ()
 
-  private def serialize(items: Map[String, String]): ByteBuffer = {
-    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
-    val oos                           = new ObjectOutputStream(stream)
-    oos.writeObject(items)
-    oos.close
-
-    ByteBuffer.wrap(stream.toByteArray)
-  }
-
-  private def deserialize(bytes: ByteBuffer): Map[String, String] = {
-
-    val ois      = new ObjectInputStream(new ByteArrayInputStream(bytes.array()))
-    val response = ois.readObject().asInstanceOf[Map[String, String]]
-    ois.close()
-
-    response
-  }
 }
 
 object KvStateMachine {
---
+++ src/main/scala/raft4s/demo/kvstore/utils/ObjectSerializer.scala
@@ -0,0 +1,25 @@
+package raft4s.demo.kvstore.utils
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+import java.nio.ByteBuffer
+
+object ObjectSerializer {
+
+  def serialize(items: Map[String, String]): ByteBuffer = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos                           = new ObjectOutputStream(stream)
+    oos.writeObject(items)
+    oos.close
+
+    ByteBuffer.wrap(stream.toByteArray)
+  }
+
+  def deserialize(bytes: ByteBuffer): Map[String, String] = {
+
+    val ois      = new ObjectInputStream(new ByteArrayInputStream(bytes.array()))
+    val response = ois.readObject().asInstanceOf[Map[String, String]]
+    ois.close()
+
+    response
+  }
+}
